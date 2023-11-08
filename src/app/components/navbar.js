9a41c208a9b2f86ce7e82a41be0a3d52ef03fb26
mpersaud0 <mikey.walkerrr@gmail.com>
2023-11-08 14:42:31 -0500

    fixed bg image and further progress for detail packages page

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -4,17 +4,17 @@ export default function NavBar() {
   return (
     <nav className='flex flex-row m-auto font-poppins items-center pt-12 pb-12 border-b-4 border-b-red-600'>
       <div className='pr-56'>
-        <Link className='p-5 font-extrabold' href="/home">Home</Link>
-        <Link className='p-5 font-extrabold' href="/services">Services</Link>
-        <Link className='p-5 font-extrabold' href="/packages">Packages & Pricing</Link>
+        <Link className='p-5 font-extrabold' href="/">Home</Link>
+        <Link className='p-5 font-extrabold' href="/work">Our Work</Link>
+        <Link className='p-5 font-extrabold' href="/membership">Monthly Membership</Link>
       </div>
-      <div className='relative max-w-[100%]'>
+      <div className='relative max-w-full'>
         <img className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-12 max-w-xs' src='/images/logo.webp' alt='' />
       </div>
       <div className='pl-56'>
-        <Link className='p-5 font-extrabold' href="/Membership">Monthly Membership</Link>
-        <Link className='p-5 font-extrabold' href="/Gallery">Gallery</Link>
-        <Link className='p-5 font-extrabold' href="/FAQ">FAQ</Link>
+        <Link className='p-5 font-extrabold' href="/ceramiccoating">Ceramic Coating</Link>
+        <Link className='p-5 font-extrabold' href="/detailpackages">Detail Packages</Link>
+        <Link className='p-5 font-extrabold' href="/polishingpackages">Polishing Packages</Link>
       </div>
     </nav>
   );
